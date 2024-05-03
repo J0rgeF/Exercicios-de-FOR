f386c49cfe96f412d27cdcdb2de2ed770e2c8b54
Lourenccc <167427744+Lourenccc@users.noreply.github.com>
2024-05-03 10:46:48 -0300

    Create ex2.c

---
+++ ex2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+
+    //2) Faça um algoritmo que exiba todos números de 100 a 1.
+
+    for(int i = 100; i <= 100 && i >0; i--){
+        printf("%i\n", i);
+    }
+
+return 0; 
+}
