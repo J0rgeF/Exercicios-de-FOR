4e3b006f727719af89d7725e87ad025e0c82b3b2
Lourenccc <167427744+Lourenccc@users.noreply.github.com>
2024-05-03 10:42:08 -0300

    Create ex1.c

---
+++ ex1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+
+//1) Faça um algoritmo que exiba todos números de 1 a 100.
+
+    for(int i=1; i <= 100; i++){
+        printf("%i\n", i);
+    }
+
+return 0;
+}
