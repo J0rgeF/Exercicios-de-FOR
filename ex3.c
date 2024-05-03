402035c87de9e8faea9c6d10b9adf58b7d256195
J0rgef <jorgemiguelzito@gmail.com>
2024-05-03 16:12:51 +0100

    Removing unnecessary code + Fixing Exercise 3

--- ex3.c
+++ ex3.c
@@ -1,1 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+int main(){
+
+    /*3) Crie um algoritmo que exiba todos os números múltiplos
+    de 5 no intervalo de 1 a 500.*/
+
+    for(int i=1; i <= 500; i++){
+            if(i % 5 == 0){
+                printf("%i\n", i);
+            }
+    }
+
+return 0;
+}
--- ex4.c
+++ ex4.c
@@ -6,11 +6,8 @@ int main(){
 /*4) Crie um algoritmo que exiba todos os números pares
 entre 240 e 730 inclusive.*/
 
-int conta;
-
     for(int i=240; i<=730; i++){
-        conta = i % 2;
-            if(conta == 0){
+            if(i % 2 == 0){
                 printf("%i\n", i);
             }
     }
